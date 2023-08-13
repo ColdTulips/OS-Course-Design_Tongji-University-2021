857d16620e028239954aa075647b593cb0c2b0f3
ColdTulips <128375162+ColdTulips@users.noreply.github.com>
2023-08-13 12:03:48 +0800

    Add files via upload

---
+++ kernel/bio.c
@@ -0,0 +1,211 @@
+// Buffer cache.
+//
+// The buffer cache is a linked list of buf structures holding
+// cached copies of disk block contents.  Caching disk blocks
+// in memory reduces the number of disk reads and also provides
+// a synchronization point for disk blocks used by multiple processes.
+//
+// Interface:
+// * To get a buffer for a particular disk block, call bread.
+// * After changing buffer data, call bwrite to write it to disk.
+// * When done with the buffer, call brelse.
+// * Do not use the buffer after calling brelse.
+// * Only one process at a time can use a buffer,
+//     so do not keep them longer than necessary.
+
+
+#include "types.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "riscv.h"
+#include "defs.h"
+#include "fs.h"
+#include "buf.h"
+
+#define NBUCKET 13 
+struct {
+  struct spinlock buflock;
+  struct spinlock biglock;
+  struct spinlock bucketlock[NBUCKET];
+  struct buf buf[NBUF];
+  struct buf bucket[NBUCKET];
+  int usedbuf;
+  // Linked list of all buffers, through prev/next.
+  // Sorted by how recently the buffer was used.
+  // head.next is most recent, head.prev is least.
+  // struct buf head;
+} bcache;
+
+int hash(int blockno)
+{
+  return blockno % NBUCKET;
+}
+
+void
+binit(void)
+{
+  //struct buf *b;
+  initlock(&bcache.buflock,"bcache_buf");
+  initlock(&bcache.biglock, "bcache_big");
+  for(int i=0;i<NBUCKET;i++)
+  {
+    initlock(&bcache.bucketlock[i],"bcache_bucket");
+  }
+  for(int i=0;i<NBUF;i++)
+  {
+    initsleeplock(&bcache.buf[i].lock, "buffer");
+  }
+  bcache.usedbuf = 0;
+}
+
+// Look through buffer cache for block on device dev.
+// If not found, allocate a buffer.
+// In either case, return locked buffer.
+static struct buf*
+bget(uint dev, uint blockno)
+{
+  struct buf *b;
+  int id = hash(blockno);
+  struct buf *minb=0, *minpre;
+  uint mintimestamp = __INT32_MAX__;
+
+  acquire(&bcache.bucketlock[id]);
+  for(b = &(bcache.bucket[id]);b!=0;b=b->next)
+  {
+    if(b->dev == dev && b->blockno == blockno)
+    {
+      b->refcnt++;
+      release(&bcache.bucketlock[id]);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+  
+  acquire(&bcache.buflock);
+  if(bcache.usedbuf<NBUF)
+  {
+    b = &(bcache.buf[bcache.usedbuf]);
+    bcache.usedbuf++;
+    release(&bcache.buflock);
+    b->dev = dev;
+    b->blockno = blockno;
+    b->valid = 0;
+    b->refcnt = 1;
+    //Insert
+    b->next = bcache.bucket[id].next;
+    bcache.bucket[id].next = b;
+    release(&bcache.bucketlock[id]);
+    acquiresleep(&b->lock);
+    return b;
+  }
+  release(&bcache.buflock);
+  release(&bcache.bucketlock[id]);
+
+  acquire(&bcache.biglock);
+  int tempid = id;
+  for(int i = 0; i < NBUCKET; i++) {
+      mintimestamp = -1;
+      acquire(&bcache.bucketlock[tempid]);
+      struct buf* pre;
+      for(pre = &bcache.bucket[tempid], b = pre->next; b!=0; pre = b, b = b->next) {
+          
+          if(tempid == id && b->dev == dev && b->blockno == blockno){
+              b->refcnt++;
+              release(&bcache.bucketlock[tempid]);
+              release(&bcache.biglock);
+              acquiresleep(&b->lock);
+              return b;
+          }
+
+          if(b->refcnt == 0 && b->timestamp < mintimestamp) {
+              minb = b;
+              minpre = pre;
+              mintimestamp = b->timestamp;
+          }
+      }
+      // find an unused block
+      if(minb) {
+          minb->dev = dev;
+          minb->blockno = blockno;
+          minb->valid = 0;
+          minb->refcnt = 1;
+          // if block in another bucket, we should move it to correct bucket
+          if(tempid != id) {
+              minpre->next = minb->next;    // remove block
+              release(&bcache.bucketlock[tempid]);
+              tempid = id;  // the correct bucket index
+              acquire(&bcache.bucketlock[tempid]);
+              minb->next = bcache.bucket[tempid].next;    // move block to correct bucket
+              bcache.bucket[tempid].next = minb;
+          }
+          release(&bcache.bucketlock[tempid]);
+          release(&bcache.biglock);
+          acquiresleep(&minb->lock);
+          return minb;
+      }
+      release(&bcache.bucketlock[tempid]);
+      tempid = hash(tempid + 1);
+  }
+
+  panic("bget: no buffers");
+}
+
+// Return a locked buf with the contents of the indicated block.
+struct buf*
+bread(uint dev, uint blockno)
+{
+  struct buf *b;
+
+  b = bget(dev, blockno);
+  if(!b->valid) {
+    virtio_disk_rw(b, 0);
+    b->valid = 1;
+  }
+  return b;
+}
+
+// Write b's contents to disk.  Must be locked.
+void
+bwrite(struct buf *b)
+{
+  if(!holdingsleep(&b->lock))
+    panic("bwrite");
+  virtio_disk_rw(b, 1);
+}
+
+// Release a locked buffer.
+// Move to the head of the most-recently-used list.
+void
+brelse(struct buf *b)
+{
+  if(!holdingsleep(&b->lock))
+    panic("brelse");
+
+  releasesleep(&b->lock);
+
+  int id = hash(b->blockno);
+  acquire(&bcache.bucketlock[id]);
+  b->refcnt--;
+  if (b->refcnt == 0) {
+    b->timestamp = ticks;
+  }
+  
+  release(&bcache.bucketlock[id]);
+}
+
+void
+bpin(struct buf *b) {
+  int id = hash(b->blockno);
+  acquire(&bcache.bucketlock[id]);
+  b->refcnt++;
+  release(&bcache.bucketlock[id]);
+}
+
+void
+bunpin(struct buf *b) {
+  int id = hash(b->blockno);
+  acquire(&bcache.bucketlock[id]);
+  b->refcnt--;
+  release(&bcache.bucketlock[id]);
+}
